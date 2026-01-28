fe4ca6b08b71099493761de451215680fb0693eb
jannu <jan.tahvonen@gmail.com>
2026-01-28 13:53:51 +0200

    Open modal n fix scss

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,23 @@
 <script setup>
-import { ref, computed } from 'vue'
-let i = 1;
-let items = ref([
-  {id: i++, text:'Sai', isDone: true},
-  {id: i++, text:'Piim', isDone: false},
-  {id: i++, text:'Viin', isDone: true},
-  {id: i++, text:'Õlu', isDone: false}
-    ])
+import { ref } from 'vue';
  
-const newItem = ref('')
+let modal1Active = ref(false);
  
-let doneItems = computed(() => {
-    return items.value.filter(i => i.isDone);
-});
- 
-const addItem = () => {
-  if (newItem.value.trim() !== '') {
-    items.value.push({id: i++, text: newItem.value.trim(), isDone: false})
-  }
-  newItem.value = ''
-}
 </script>
  
 <template>
-  <div class="container">
-    <div class="field has-addons mt-2">
-      <div class="control is-expanded">
-        <input
-          class="input"
-          type="text"
-          v-model="newItem"
-          @keypress.enter="addItem"
-        />
-      </div>
-      <div class="control">
-        <button class="button is-info" @click="addItem">
-          Add Item
-        </button>
-      </div>
+    <div class="container">
+      <button class="button is-primary" @click="modal1Active=true">Open modal</button>
+        <div :class="modal1Active ? 'modal is-active' : 'modal'">
+            <div class="modal-background" @click="modal1Active=false"></div>
+            <div class="modal-content">
+                <p class="image is-4by3">
+                    <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+                </p>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="modal1Active=false"></button>
+        </div>
     </div>
- 
-    <div class="content">
-      <h1>All Items</h1>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          {{ item.text }}
-          <input type="checkbox" v-model="item.isDone" />
-        </li>
-      </ul>
-    </div>
- 
-<div class="content">
-  <h1>Done Items</h1>
-  <ul>
-    <li v-for="item in doneItems" :key="item.id">
-      {{ item.text }}
-      <input type="checkbox" v-model="item.isDone" />
-    </li>
-  </ul>
-</div>
-  </div>
 </template>
+ 
+<style></style>
