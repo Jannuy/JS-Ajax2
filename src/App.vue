ed2243dcd1d58d2002c57a3a326f2748cf35792b
jannu <jan.tahvonen@gmail.com>
2026-04-01 13:12:50 +0300

    Chuck norris quotes added

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <li><router-link to="/">ToDo</router-link></li>
         <li><router-link to="/modals">Modals</router-link></li>
         <li><router-link to="/messages">Messages</router-link></li>
+        <li><router-link to="/chucknorris">Chuck Norris</router-link></li>
         <li><router-link to="/rickandmorty">Rick and Morty</router-link></li>
       </ul>
     </div>
@@ -12,4 +13,5 @@
     <router-view />
   </div>
 </template>
+ 
  
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import ToDo from "./pages/ToDo.vue";
 import Modals from "./pages/Modals.vue";
 import Messages from "./pages/Messages.vue";
 import RickAndMorty from "./pages/RickAndMorty.vue";
+import ChuckNorris from "./pages/ChuckNorris.vue";
  
-const router = createRouter({
+export default createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/", component: ToDo },
     { path: "/modals", component: Modals },
     { path: "/messages", component: Messages },
+    { path: "/chucknorris", component: ChuckNorris },
     { path: "/rickandmorty", component: RickAndMorty },
   ],
 });
  
-export default router;
