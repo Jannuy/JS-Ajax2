191a9279db6880207a5c0dc1083a6cd103bdb288
jannu <jan.tahvonen@gmail.com>
2026-04-01 13:01:43 +0300

    paar faili sisu muudetud

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App);
-app.mount('#app');
+import { createRouter, createWebHashHistory } from 'vue-router'
+import ToDo from './pages/ToDo.vue'
+import Modals from './pages/Modals.vue'
+import Messages from './pages/Messages.vue'
+import RickAndMorty from './pages/RickAndMorty.vue'
+ 
+const routes = [
+  { path: '/', component: ToDo },
+  { path: '/modals', component: Modals },
+  { path: '/messages', component: Messages },
+  { path: '/rickandmorty', component: RickAndMorty },
+]
+ 
+export default createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+ 
